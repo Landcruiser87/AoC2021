252a57d95496a53f4a430420463f129afe3d0363
Landcruiser87 <andyheroy@gmail.com>
2021-12-02 12:06:27 -0600

    cleanup day1

--- day1/day1.py
+++ day1/day1.py
@@ -5,15 +5,14 @@ import numpy as np
 #	and returns whether or not the the value preceeding 
 #	is increasing or decreasing.
 
-#2. Figure out how quickly the depth increase or decrease
-#3. End Goal: Count how many times a depth measurement increases 
+#2. End Goal: Count how many times a depth measurement increases 
 #	from the pervious measurement.
 
 #.\day1\
 depths = np.loadtxt('.\day1\data.txt', dtype=int)
 res_dict = {"Increase":0, "Decrease":0}
 
-def count_slopes(depths):
+def count_slopes(depths:np.array)->int:
 	for i in range(1, len(depths)):
 		if depths[i] > depths[i-1]:
 			res_dict["Increase"] += 1
@@ -28,13 +27,13 @@ print(f'Problem 1 Solution: {count_slopes(depths)}')
 ##Problem 2
 #Repeat the above calculations but this time. 
 # with a 3 measurement sliding window
-# So sum the 3 of the sliding window, then compare 
-# to the previous window shift back 1 unit. 
+# So sum the 3 wide sliding window, then compare 
+# to the previous window shifted back 1 unit. 
 
 from numpy.lib.stride_tricks import sliding_window_view
 res_dict = {"Increase":0, "Decrease":0}
 
-def slide_window_count_slopes(depths:np.array):
+def slide_window_count_slopes(depths:np.array)->int:
 
 	windowed_data = sliding_window_view(depths, window_shape = 3)
 	for i in range(len(windowed_data)):
