8b63bf4c258e7719d9a22b6243aef9b8d31c1568
Landcruiser87 <andyheroy@gmail.com>
2021-12-08 15:55:39 -0600

    finshed day 8 part A.  Part B.  Well.  Its hard.

--- day8/day8.py
+++ day8/day8.py
@@ -80,4 +80,86 @@ def run_part_A()->int:
 				count_dict[8] += 1
 	return sum(count_dict.values())
 
-print(f'Solution to part A: {run_part_A()}')
+# print(f'Solution to part A: {run_part_A()}')
+
+
+#Part B
+#After some careful deduction, we've discovered the signal patterns layout.
+#Which is the following. 
+#  dddd
+# e    a
+# e    a
+#  ffff
+# g    b
+# g    b
+#  cccc
+#After decoding the outputs with the above layout, 
+#! We'll need to join the decoded outputs into a single number
+#The tricky part is we have to build the digit codex for every entry in the input
+#FUCK
+#Alright.  First things first.  
+# We know [1, 4, 7, 8] are the only ones that use unique strings
+
+
+def test_data_load()->np.ndarray:
+	# Load data
+	with open('test_data.txt', 'r') as f:
+		data = f.read().split('\n')
+		inputs = [data[i].split("|")[0].strip() for i in range(len(data))]
+		outputs = [data[i].split("|")[1].strip() for i in range(len(data))]
+	return inputs, outputs
+
+# wiring_dict = {
+# 	8:'acedgfb',
+# 	5:'cdfbe',
+# 	2:'gcdfa',
+# 	3:'fbcad',
+# 	7:'dab',
+# 	9:'cefabd',
+# 	6:'cdfgeb',
+# 	4:'eafb',
+# 	0:'cagedb',
+# 	1:'ab'}
+
+def make_wiring_diagram(input_idx:int, in_str:str)->str:
+	
+	wiring_dict = {x:set() for x in range(10)}
+
+	for inp in in_str.split(' '):
+		inp_len = len(inp)
+
+
+	# First thing is to isolate the lengths of 
+	# [1, 4, 7 8] and assign those to the wiring_dict. 
+	# Then we need to calculate the others. 
+		#Not sure how to do this
+
+
+
+inputs, outputs  = test_data_load()
+# outputs = ['cdfeb fcadb cdfeb cdbaf', '']
+def get_num_fromdict(str_num:str)->int:
+	for k, v in wiring_dict.items():
+		if set(str_num) == set(v):
+			return k
+
+results = []
+for idx, out in enumerate(outputs):
+	res = []
+	wiring_dict = make_wiring_diagram(inputs[idx])
+	for o in out.split(" "):
+		num = get_num_fromdict(o, wiring_dict)
+		if num != None:
+			res.append(num)
+
+	#Join them together	
+	if len(res) > 1:
+		res = int("".join([str(x) for x in res]))
+	elif len(res) == 1:
+		res = res[0]
+	else:
+		continue
+	
+	results.append(res)
+	
+print(sum(results))
