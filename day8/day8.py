d5baa7b6a736f6b4322058c719459e68c46cbc13
Landcruiser87 <andyheroy@gmail.com>
2021-12-10 09:06:58 -0600

    finally finished day 8 - Whoo hooooo

--- day8/day8.py
+++ day8/day8.py
@@ -32,7 +32,7 @@
 
 # The image is static in size.  
 # Wires are connected to strings a-g
-# Numbers [1, 4, 7, 8] all use unique string id's
+# Numbers [1, 4, 7, 8] all use unique length string id's
 	#ie, these are the keys to deducing the rest i'd bet
 # Image count for turning on
 #0:6- count repeated
@@ -50,6 +50,8 @@
 # [10 unique signal patterns] | [four digit output]
 
 import numpy as np
+import itertools
+
 
 # ./day8/
 def data_load()->np.ndarray:
@@ -84,82 +86,109 @@ def run_part_A()->int:
 
 
 #Part B
-#After some careful deduction, we've discovered the signal patterns layout.
-#Which is the following. 
-#  dddd
-# e    a
-# e    a
-#  ffff
-# g    b
-# g    b
-#  cccc
-#After decoding the outputs with the above layout, 
-#! We'll need to join the decoded outputs into a single number
-#The tricky part is we have to build the digit codex for every entry in the input
+
+#Now that we've figured out the first mapping, its time to do so for the rest of the outputs
+#Our task:
+#	1.	Find the mapping of each input for each output / display
+#	2.  Decode and sum all the displays
+
+#The tricky part is we have to build the digit codex for every code in the input
 #FUCK
-#Alright.  First things first.  
-# We know [1, 4, 7, 8] are the only ones that use unique strings
 
+# ./day8/ 
+def data_load_partB()->list:
+	with open('./day8/data.txt', 'r') as f:
+		data = f.read().splitlines()
+	return data
 
-def test_data_load()->np.ndarray:
-	# Load data
-	with open('test_data.txt', 'r') as f:
-		data = f.read().split('\n')
-		inputs = [data[i].split("|")[0].strip() for i in range(len(data))]
-		outputs = [data[i].split("|")[1].strip() for i in range(len(data))]
-	return inputs, outputs
+#Initial wiring board Layout from p1
+    # Patterns following the following configuration:
+    #  aaaa 
+    # b    c
+    # b    c
+    #  dddd 
+    # e    f
+    # e    f
+    #  gggg 
 
-# wiring_dict = {
-# 	8:'acedgfb',
-# 	5:'cdfbe',
-# 	2:'gcdfa',
-# 	3:'fbcad',
-# 	7:'dab',
-# 	9:'cefabd',
-# 	6:'cdfgeb',
-# 	4:'eafb',
-# 	0:'cagedb',
-# 	1:'ab'}
+wiring_dict = {		#Length
+	"0": 'abcefg',	#6
+	"1": 'cf',		#2
+	"2": 'acdeg',	#5
+	"3": 'acdfg',	#5
+	"4": 'bcdf',	#4
+	"5": 'abdfg',	#5
+	"6": 'abdefg',	#6
+	"7": 'acf',		#3
+	"8": 'abcdefg',	#7
+	"9": 'abcdfg',	#6
+	}
 
-def make_wiring_diagram(input_idx:int, in_str:str)->str:
+def sort_alpha(input_str:str)->str:
+	return "".join(sorted(input_str))
+
+def switch_out_char(in_code:str, maps:dict)->str:
+	return sort_alpha(maps[code] for code in in_code)
+
+def decoder_ring(in_codes:str, wiring_dict:dict, update_dict:dict, known_code:list)->list:
 	
-	wiring_dict = {x:set() for x in range(10)}
-
-	for inp in in_str.split(' '):
-		inp_len = len(inp)
-
-
-	# First thing is to isolate the lengths of 
-	# [1, 4, 7 8] and assign those to the wiring_dict. 
-	# Then we need to calculate the others. 
-		#Not sure how to do this
-
-
-
-inputs, outputs  = test_data_load()
-# outputs = ['cdfeb fcadb cdfeb cdbaf', '']
-def get_num_fromdict(str_num:str)->int:
-	for k, v in wiring_dict.items():
-		if set(str_num) == set(v):
-			return k
-
-results = []
-for idx, out in enumerate(outputs):
-	res = []
-	wiring_dict = make_wiring_diagram(inputs[idx])
-	for o in out.split(" "):
-		num = get_num_fromdict(o, wiring_dict)
-		if num != None:
-			res.append(num)
-
-	#Join them together	
-	if len(res) > 1:
-		res = int("".join([str(x) for x in res]))
-	elif len(res) == 1:
-		res = res[0]
-	else:
-		continue
+	#Early stop if we've found all the codes
+	if len(in_codes) == 0:
+		return known_code
 	
-	results.append(res)
-	
-print(sum(results))
+	#first code
+	in_code = in_codes[0]
+
+	for dict_key, dict_code in wiring_dict.items(): 
+		#if key identified or length of dict_code isn't equal to the in_code we're deciphering.
+		if dict_key in known_code or len(dict_code) != len(in_code):
+			continue
+		
+		#Get the letters from incodes that are unidentified. 
+		wires_to_map = [letter for letter in in_code if letter not in update_dict.keys()]
+
+		#Get the letters from wiring_dict that are unindentified.
+		wires_to_switch = [letter for letter in dict_code if letter not in update_dict.values()]
+
+		# Check to make sure the wires lengths match. 
+		if len(wires_to_map) != len(wires_to_switch):
+			continue
+		
+		perms = itertools.permutations(wires_to_map)
+		
+		for perm in perms:
+			maps = {k:v for k,v in zip(perm, wires_to_switch)}
+
+			if switch_out_char(in_code, {**update_dict, **maps}) != dict_code:
+				continue
+		
+			result = decoder_ring(in_codes[1:], wiring_dict, {**update_dict, **maps}, known_code+[dict_key])
+
+			if result:
+				return result
+	return None
+
+def run_part_B()->int:
+	data = data_load_partB()
+	result = 0
+
+	for line in data:
+		#Split the line into input/output
+		inputs, outputs = line.split(" | ")
+		in_codes = inputs.split()
+		out_codes = outputs.split()
+		
+		#sort them by length
+		in_codes = sorted([sort_alpha(x) for x in in_codes], key=len)
+
+		#Make/Update the wiring dict
+		found = decoder_ring(in_codes, wiring_dict, {}, [])
+		if not found:
+			print("Failed to decode")
+			exit()		
+		final_mapping = {k:v for k,v in zip(in_codes, found)}
+		
+		result += int("".join(final_mapping[sort_alpha(code)] for code in out_codes))
+	return result
+
+print(f'Solution to part B: {run_part_B()}')
