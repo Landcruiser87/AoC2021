d56b36f23e874b027ddbf543a20183fed762e2f1
Landcruiser87 <andyheroy@gmail.com>
2021-12-13 08:25:03 -0600

    Finished day 10

--- day10/day10.py
+++ day10/day10.py
@@ -33,6 +33,13 @@ point_dict = {
 	">": 25137,
 }
 
+autoc_dict = {
+	")": 1,
+	"]": 2,
+	"}": 3,
+	">": 4,
+}
+
 def run_part_A()->int:
 	data = load_data()
 	score = 0
@@ -51,4 +58,41 @@ def run_part_A()->int:
 	
 	return score
 
-print(f"Solution for Part A: {run_part_A()}")
+# print(f"Solution for Part A: {run_part_A()}")
+
+# Part 2
+def run_part_B()->int:
+	data = load_data()
+	final_scores = []
+	
+	for line in data:
+		score = 0
+		char_list = []
+		for char in line:
+			if char not in point_dict.keys():
+				char_list.append(char)
+			else:
+				if char == pair_dict[char_list[-1]]:
+					char_list.pop()
+				else:
+					char_list = []
+					break
+		if char_list:
+			line_score = [pair_dict[char] for char in char_list[::-1]]
+			for auto_key in line_score:
+				score = (score * 5) + autoc_dict[auto_key]
+			final_scores.append(score)
+	return sorted(final_scores)[len(final_scores)//2]
+
+print(f'Solution for Part B: {run_part_B()}')
+
+#1 [({(<(())[]>[[{[]{<()<>>
+#2 [(()[<>])]({[<{<<[]>>(
+#3 {([(<{}[<>[]}>{[]{[(<()>
+#4 (((({<>}<{<{<>}{[]{[]{}
+#5 [[<[([]))<([[{}[[()]]]
+#6 [{[{({}]{}}([{[{{{}}([]
+#7 {<[[]]>}<{[{[{[]{()[[[]
+#8 [<(<(<(<{}))><([]([]()
+#9 <{([([[(<>()){}]>(<<{{
+#10 <{([{{}}[<[[[<>{}]]]>[]]
