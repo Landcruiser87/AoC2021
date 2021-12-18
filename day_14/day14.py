81cc7d3e5fdc2ee53295a0e2245df03ee1462138
Landcruiser87 <andyheroy@gmail.com>
2021-12-17 23:04:16 -0600

    still stuck day 14

--- day_14/day14.py
+++ day_14/day14.py
@@ -10,12 +10,10 @@
 # Each rule insertion happens simultaneously
 	#Meaning each step, only their pairs at the beginning are considered. 
 
-import numpy as np
-from collections import Counter
-
+from collections import Counter, defaultdict
 # ./day_14/
 def data_load()-> (dict, str):
-	with open('./day_14/data.txt') as f:
+	with open('test_data.txt') as f:
 		data = f.read().splitlines()
 		data = [line.split(" -> ") for line in data]
 		polymer = data.pop(0)[0]
@@ -47,6 +45,7 @@ def pair_insert(polymer:str, rules:dict)-> str:
 def run_part_A()-> int:
 	polymer, rules = data_load()
 	poly_update = pair_insert(polymer, rules)
+
 	for i in range(1, 10):
 		poly_update = pair_insert(poly_update, rules)
 
@@ -55,4 +54,46 @@ def run_part_A()-> int:
 	ans = max(dict_counter.values()) - min(dict_counter.values())
 	return ans
 
-print(f"Solution for Part 1: {run_part_A()}")
+# print(f"Solution for Part A: {run_part_A()}")
+
+#Turns out you can't just increase the range and compute the same.  List values get way too big. 
+# Soooooooo.  time for a bunch of Counters / dictionaries
+
+def pair_insert_dos(dict_counter:dict, rules:dict)-> dict:
+	#Only want to pass counters back and forth.  Can't compute string as it gets ginormous
+
+	freq_dict = dict_counter.copy()
+	for pair in freq_dict.keys():
+		if pair in rules:
+			#Insert the rule, but update the dict_counter.
+			dict_counter[pair[0] + rules[pair]] += 1
+			dict_counter[rules[pair] + pair[1]] += 1
+
+	return dict_counter
+
+def run_part_B()-> int:
+	polymer, rules = data_load()
+
+	#Initialize two counter dicts
+	pair_counts, freq_dict = defaultdict(int), defaultdict(int)
+
+	#start the madness with the initial pairs.  Add them to the defaultdict. 
+	for x in range(len(polymer)-1):
+		pair_counts[polymer[x:x+2]] += 1
+	
+
+	for i in range(40):
+		#update the freq_dict for every step
+		freq_dict = pair_insert_dos(pair_counts, rules)
+
+
+	final_counts = defaultdict(int)
+	for pairs in freq_dict.keys():
+		for ch in pairs:
+			final_counts[ch] += freq_dict[pairs]
+
+
+	ans = max(counts.values()) - min(counts.values())
+	return ans
+
+print(f"Solution for Part B: {run_part_B()}")
